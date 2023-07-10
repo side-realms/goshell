d4c4fbd1db8698abd288abae0f8b4382c7919887
side_realms <87800512+side-realms@users.noreply.github.com>
2023-07-11 00:44:45 +0900

    オプション追加，cd 追加，顔文字追加

--- hello.go
+++ hello.go
@@ -2,33 +2,91 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
+	"os/user"
 	"strings"
 )
 
-func exe(input string) {
-	input = strings.TrimRight(input, "\n")
-	out, err := exec.Command(input).Output()
-	if err != nil {
-		fmt.Fprintln(os.Stdout, "ERROR:")
-		fmt.Fprintln(os.Stderr, err)
-	} else {
-		fmt.Fprintln(os.Stdout, string(out))
+func ChangeDirectory(inputs []string) error {
+	if len(inputs) < 2 {
+		return errors.New("Add path!")
 	}
+	return os.Chdir(inputs[1])
+}
+
+func SuperUser(inputs []string) {
+	cmd := exec.Command("su")
+	cmd.Stdin = strings.NewReader("ubuntu" + "\n")
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Print("\x1b[31m", "(#`-´)<ENTER PASSWORD!!!!! : ")
+	//pass, _ := reader.ReadString('\n')
+
+	//cmd := exec.Command(inputs[0])
+	//cmd.Stdin = strings.NewReader("ubuntu" + "\n")
+
+	//return cmd.Run()
+}
+
+func exe(input string) error {
+	input = strings.TrimRight(input, "\n")
+	inputs := strings.Split(input, " ")
+
+	if inputs[0] == "cd" {
+		err := ChangeDirectory(inputs)
+		return err
+	}
+
+	if inputs[0] == "su" {
+		SuperUser(inputs)
+	}
+
+	if len(inputs) == 1 {
+		cmd := exec.Command(inputs[0])
+		result, err := cmd.Output()
+		fmt.Println(string(result))
+		return err
+	} else {
+		cmd := exec.Command(inputs[0], inputs[1])
+		result, err := cmd.Output()
+		fmt.Println(string(result))
+		return err
+	}
+
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print(">")
+		//dir, err := os.Getwd()
+		user, err := user.Current()
+
+		if err != nil {
+			panic(err)
+		}
+
+		if user.Uid == "0" {
+			fmt.Print("\x1b[31m", "(#`-´)< ")
+		} else {
+			fmt.Print("\x1b[32m", "(#`-´)< ")
+		}
 		// Read the keyboad input.
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
-		exe(input)
-		//fmt.Fprintln(os.Stdout, input+"keroro")
+
+		if err = exe(input); err != nil {
+			fmt.Fprintln(os.Stdout, "ERROR")
+			fmt.Fprintln(os.Stderr, err)
+		}
 	}
 }
